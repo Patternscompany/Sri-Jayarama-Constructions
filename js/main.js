9ea3587698777149c1da19df9b53505e912f806b
Patternscompany <pandureddypatterns@gmail.com>
2025-11-25 12:34:36 +0530

    pp

--- js/main.js
+++ js/main.js
@@ -26,6 +26,46 @@
     });
 
 
+    // testimonial carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-right"></i>',
+            '<i class="fa fa-angle-left"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:2
+            }
+        }
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 5,
+        time: 2000
+    });
+
    // Back to top button
    $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
